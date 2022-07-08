cffb42a853a61f86c84309470f88e80591185164
[Pablo_Rossi] <[pablo.a.rossi83@gmail.com]>
2022-07-08 16:08:13 -0300

    first commit

---
+++ e-commerce_pablo_rossi/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>e-commerce Pablo Rossi</h1>
+      <p>esta es mi primera app react</p>
+    </div>
+  );
+}
+
+export default App;
